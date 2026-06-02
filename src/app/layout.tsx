eb3217c5a5e823829d65062802e0764ed041a8ad
Saranyo Ray <saranyo.ray2022@vitstudent.ac.in>
2026-06-03 04:19:42 +0530

    vercel
    vercel speedinsights and analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 ﻿import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Cormorant_Garamond, Nunito, Noto_Serif_Bengali } from "next/font/google";
 import { contactInfo, contactLinks, siteConfig } from "@/data/site-data";
 import "./globals.css";
@@ -109,6 +111,8 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
         />
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
